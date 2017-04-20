d38632a6b6e96811267eda35354b003b3a6c6e9f
BBreton112 <bbreton112@gmail.com>
2017-04-20 14:49:59 -0400

    Add files via upload

--- modele/game/game_objects/GameObjectType.java
+++ modele/game/game_objects/GameObjectType.java
@@ -3,15 +3,21 @@ package modele.game.game_objects;
 import javafx.scene.paint.Color;
 
 public enum GameObjectType {
-    HUMAN(Color.RED), AI(Color.BLUE), ENEMY(Color.BLACK), VOID(Color.GRAY);
+    HUMAN(Color.RED, "images/joueur.png"), AI(Color.BLUE, "images/joueur.png"), ENEMY(Color.BLACK, "images/obstacle.png"), VOID(Color.GRAY, "");
 
     Color color = null;
+    String url = "";
 
-    GameObjectType(Color color) {
+    GameObjectType(Color color, String url) {
         this.color = color;
+        this.url = url;
     }
 
     public Color getColor() {
         return color;
+    }
+
+    public String getURL() {
+        return url;
     }
 }
