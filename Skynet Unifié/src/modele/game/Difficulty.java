5192f35e794bb1ee9ab688a15519412bcf62be9e
BBreton112 <bbreton112@gmail.com>
2017-05-03 16:55:12 -0400

    Update Difficulty.java

--- Skynet Unifié/src/modele/game/Difficulty.java
+++ Skynet Unifié/src/modele/game/Difficulty.java
@@ -4,9 +4,9 @@ package modele.game;
 
 /**
  *
- * @author B�nange Breton
+ * @author Bénange Breton
  *
- *	Cette classe contient les diff�rentes difficult�s du jeu et tous les param�tres de jeu que la difficult� affecte.
+ *	Cette classe contient les différentes difficultés du jeu et tous les paramètres de jeu que la difficulté affecte.
  */
 public enum Difficulty {
 	VERY_EASY("Très facile", 1, false, true, 0, 0, 0, 0, true, false, 0.5f, 0),
@@ -18,18 +18,18 @@ public enum Difficulty {
 
 	private String name = "";
 
-	private boolean startAcceleration, 				//Si un ennemi a une acc�l�ration
-	 				constantVelocity,				//Si tous les ennemis ont la m�me vitesse constante
-	 				cycleLanes,						//Si les ennemis apparaissent selon un cycle de rang�es
-	 				cycleBackAndForth;				//Si on alterne mont�e/descente dans les rang�es
+	private boolean startAcceleration, 				//Si un ennemi a une accélération
+	 				constantVelocity,				//Si tous les ennemis ont la même vitesse constante
+	 				cycleLanes,						//Si les ennemis apparaissent selon un cycle de rangées
+	 				cycleBackAndForth;				//Si on alterne mont�e/descente dans les rangées
 
-	private float startVelocityBias,				//La probabilit� qu'un ennemi ait une v�locit� au d�part
-					selfRotationBias,				//La probabilit� qu'un ennemi tourne sur lui m�me
-					selfRotationStartVelocityBias,	//La probabilit� que cette rotation ait une v�locit� angulaire de d�part
-					originRotationBias,				//La probabilit� qu'un ennemi tourne autour d'un origine
-					originRotationStarVelocityBias,	//La probabilit� que cette rotation ait une v�locit� angulaire de d�part
-					speedScaleFactor,				//Le facteur par lequel toutes les vitesses sont multipli�es
-					timeBetweenEnemiesIncrement;	//L'incr�mentation du facteur qui diminue le temps entre les ennemis
+	private float startVelocityBias,				//La probabilité qu'un ennemi ait une vélocité au départ
+					selfRotationBias,				//La probabilité qu'un ennemi tourne sur lui même
+					selfRotationStartVelocityBias,	//La probabilité que cette rotation ait une vélocité angulaire de départ
+					originRotationBias,				//La probabilité qu'un ennemi tourne autour d'un origine
+					originRotationStarVelocityBias,	//La probabilité que cette rotation ait une vélocité angulaire de départ
+					speedScaleFactor,				//Le facteur par lequel toutes les vitesses sont multipliées
+					timeBetweenEnemiesIncrement;	//L'incrémentation du facteur qui diminue le temps entre les ennemis
 
 	private Difficulty(String name, float startVelocityBias, boolean startAcceleration, boolean constantVelocity, float selfRotationBias,
 			float selfRotationStartVelocityBias, float originRotationBias, float originRotationStarVelocityBias,
