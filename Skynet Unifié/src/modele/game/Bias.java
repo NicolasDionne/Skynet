37f73d88ffba884e29c9536cba85288abaa8bdaf
BBreton112 <bbreton112@gmail.com>
2017-05-04 11:09:12 -0400

    Update Bias.java

--- Skynet Unifié/src/modele/game/Bias.java
+++ Skynet Unifié/src/modele/game/Bias.java
@@ -7,6 +7,12 @@ public interface Bias {
 
     Supplier<Double> rand = Math::random;
 
+    /**
+    *
+    *   Une simple méthode par défaut qui permet de donner un vrai/faux selon une probabilité.
+    *
+    *   @param bias un float entre 0 et 1
+    */
     default boolean testBias(float bias) {
         return (rand.get() < bias);
     }
