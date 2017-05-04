32cb3708a9e9c0f52e7688b23cd0b0db05cece07
BBreton112 <bbreton112@gmail.com>
2017-05-04 11:02:09 -0400

    Update Render.java

--- Skynet Unifié/src/modele/game/Render.java
+++ Skynet Unifié/src/modele/game/Render.java
@@ -4,6 +4,11 @@ import javafx.scene.layout.Pane;
 
 public interface Render {
 
+    /**
+    *
+    *   Méthode qui va afficher des éléments graphiques sur un Pane.
+    *
+    */
     void render(Pane pane);
 
 }
