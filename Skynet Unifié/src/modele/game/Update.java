288e7be62d6169a1b0eee5618718d42e2297ad48
BBreton112 <bbreton112@gmail.com>
2017-05-04 11:04:03 -0400

    Update Update.java

--- Skynet Unifié/src/modele/game/Update.java
+++ Skynet Unifié/src/modele/game/Update.java
@@ -3,6 +3,11 @@ package modele.game;
 
 public interface Update {
 
+    /*
+    *
+    *   Méthode qui met à jour la position et les paramètres de tous les éléments de jeu.
+    *
+    */
     void update();
 
 }
